67d82e249e7af28901a272c3726bf5871df1a4c2
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-10-10 06:08:52 +0900

    add uri.js for routing path

--- uri.js
+++ uri.js
@@ -1,4 +1,5 @@
 module.exports = {
-    'mongodbUri' : 'mongodb://127.0.0.1:27017/WebTests',
+    'mongodbUri' : 'mongodb://127.0.0.1:27017/localtest',
+    //'mongodbUri' : 'mongodb://127.0.0.1:27017/SeaUCode',
     // 'serverUrl' : 'http://localhost:3000'
 }
