11a97ee9cc98dfa095adf24f1fee7d766ddac280
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-10-10 07:51:36 +0900

    create login api

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,9 +1,66 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const jwt = require('jsonwebtoken')
+const uri = require('../../uri.js');
+const model = require('../../models/model.js');
+const crypto = require('crypto');
+
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+    const {email, password} = req.body;
+    const secret = req.app.get('jwt-secret');
+
+    model.user.find({email: email}).then(result => {
+        if(result.length == 0) {
+            throw new Error('login-fail'); // 왜 로그인이 실패했는지 알려주면 보안상으로 안좋습니다.
+        }
+        const user_info = result[0];
+
+        const hashed_password = crypto.createHmac('sha512', user_info.salt).update(password).digest('hex');
+
+        if(hashed_password === user_info.password) {
+            return new Promise((resolve, reject) => {
+                jwt.sign(
+                    {
+                        _id: user_info._id,
+                        role: user_info.role
+                    },
+                    secret,
+                    {
+                        expiresIn: '7d',
+                        subject: 'userInfo'
+                    }, (err, token) => {
+                        if (err) reject(err);
+                        resolve(token);
+                    });
+            });
+        }
+        else {
+            throw new Error('login-fail');
+        }
+    }).then(token => {
+        res.status(200).json({
+            message: "login-success",
+            token
+        });
+    }).catch(err => {
+        if(err.message === 'login-fail') {
+            res.status(403).send({message: 'login-fail'});
+        }
+        else {
+            console.log(err);
+            res.status(500).send({message: 'server-error'});
+        }
+    });
 });
 
 module.exports = router;
