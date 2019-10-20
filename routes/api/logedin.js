38845806c598feafcdbec3fe68bb9ffb5fba1f90
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-10-21 01:54:50 +0900

    adding logedin

---
+++ routes/api/logedin.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const model = require('../../models/model.js');
+
+const bodyParser = require('body-parser');
+
+const auth = require('../middleware/auth.js');
+
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+router.use(auth);
+
+router.get('/', function(req, res, next) {
+    const user_id = mongoose.Types.ObjectId(req.decoded_token._id);
+    model.user.findOne()
+        .where('_id').equals(user_id)
+        .select({'_id' : 0})
+        .select('email')
+        .select('name')
+        .then(result => {
+            res.status(200).send(result);
+        }).catch(err => {
+            if(err) {
+                res.status(500).send({message: "server error"});
+            }
+    });
+});
+
+module.exports = router;
