43328627a8634ffc2552622fecf784c6afc09e9e
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-11-07 19:41:48 +0900

    refactor logedin => logedIn

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const stylus = require('stylus');
 const config = require('./config');
-const cors = require('cors'); // TODO: CORS 를 지우십시오.
+const cors = require('cors'); // TODO: 로컬환경 테스트 용도인 CORS 를 나중에 지우십시오.
 
 /* ----
  TODO: 새로운 라우터경로를 추가해주십시오. */
 const registerRouter = require('./routes/api/user_handling/register');
 const loginRouter = require('./routes/api/login');
-const logedinRouter = require('./routes/api/logedin');
+const loggedInRouter = require('./routes/api/loggedIn');
 
 const app = express();
 
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('jwt-secret', config.key);
 
-// TODO: CORS 를 지우십시오.
+// TODO: 로컬 환경 테스트 용도인 CORS 를 나중에 지우십시오.
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
-app.use('/logedin', logedinRouter);
+app.use('/loggedIn', loggedInRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
