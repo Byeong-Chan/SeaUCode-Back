c091f980d5672c06d934ead81b9df73183611def
lee-jonghyeon-1112 <dlwhdgus5310@gmail.com>
2019-11-18 18:23:34 +0900

    modified class.js [class.js]

--- routes/api/class_handling/class.js
+++ routes/api/class_handling/class.js
@@ -10,8 +10,8 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({
     extended: false
 }));
-
 router.use(auth);
+
 
 router.post('/createClass', function(req, res, next) {
     const user_id = mongoose.Types.ObjectId(req.decoded_token._id);
@@ -80,5 +80,80 @@ router.get('/getClassInfo/:id', function(req, res, next) {
             }
     });
 });
+
+//9-2 반 id와 함께 공지목록과 그 내용 요청(GET) 받으면 반환한다.  (여기서 반 id를 parameter로 받는게 맞는지 확인 아니면 user를 통해 반 id를 찾아서 접근해야하는지
+router.get('/getNoticeList/:id',function(req,res,next){
+
+    const class_id = mongoose.Types.ObjectId(req.params.id);
+    
+    model.classroom.findOne()
+    .where('_id').equals(class_id)
+    .select('notice_list')
+    .then(result => {
+        if(result === null) throw new Error('no notice list');
+        res.status(200),json(result);
+    }).catch(err =>{
+        if(err.message === 'no notice list'){
+            res.status(400).json({message :'no notice list'});
+        }
+        else{
+            res.status(500).json({message : 'server-error'});
+        }
+    });
+});
+
+
+//11-1
+router.get('/getClassList',function(req,res,next){
+    const user_id = mongoose.Types.ObjectId(req.decoded_token._id);
+
+    model.user.find()
+    .where('_id').equals(user_id)
+    .then(result => {
+        if(result === null) throw new error ('user do not match');
+        return result.classroom_list;
+    }).then(result => {
+        res.status(200).json(result);
+    }).catch(err => {
+        if(err.message === 'user do not match'){
+            res.status(400).json({message : 'user do not match'});
+        }
+        else{
+            res.status(500).json('server-error');
+        }
+    });
+
+});
+
+
+
+//13-1 해당 반의 반 id를 파라미터에 담아서 해당 반에 속한 학생들의 대한 정보를 요청(GET) 받으면 학생들의 대한 정보(이름, 닉네임)를 반환한다.
+router.get('/getClassUserlist/:id',function(req,res,next){
+    const user_id = mongoose.Types.ObjectId(req.decoded_token._id);
+    const class_id = mongoose.Types.ObjectId(req.params.id);
+    const response = {name : [], nickname : []};
+    //질의 순서 반에서 userlist를 가져와서 user_list에 있는 user_id와 user의 _id 비교해서 이름과 닉네임 반환
+    model.classroom.findOne()
+    .where('_id').equals(class_id)
+    .then(result => {
+        if (result === null) throw new Error('no classroom exist');
+        return model.user.find.where('_id').eqauls(result.user_list);
+    }).then(result =>{
+        response.name = response.name.concat(result.name.slice(-1));
+        response.nickname = response.nickname.concat(result.nickname.slice(-1));    
+    }).then(result => {
+        res.status(200).json(response);
+    }).catch( err => {
+
+        if(err.message === 'no classroom exist'){
+            res.status(400).json({message : 'class do not exist'});
+        }
+        else{
+            res.status(500).json({message : 'server-error'});
+        }
+    });
+}); 
+
+
 
 module.exports = router;
