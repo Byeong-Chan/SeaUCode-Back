dccefb2613a4529279a60c58c0491c3e1d85553f
lee-jonghyeon-1112 <dlwhdgus5310@gmail.com>
2019-11-25 00:07:11 +0900

    20-2수정

--- routes/api/problem_handling/problems.js
+++ routes/api/problem_handling/problems.js
@@ -11,6 +11,7 @@ const auth = require('../../middleware/auth.js');
 router.use(bodyParser.urlencoded({
     extended: false
 }));
+router.use(auth);
 
 router.get('/getProblemDescription/:problemId', (req, res, next) => {
     const problem_id = req.params.problemId;
@@ -78,41 +79,64 @@ router.get('/getProblemList/category/:field/:page', (req, res, next) => {
 
 
 
-router.use(auth);
+//27-2 과제 목록을 요청(GET)받으면 해당 학생의 모든 과제목록을 반환한다.
+router.get('/getAllAssignment',function(req,res,next){
+    const user_id = mongoose.Types.ObjectId(req.decoded_token._id);
+
+    model.assignment.find()
+    .where('user_id').equals(user_id)
+    .then(result => {
+        res.status(200).json(result);
+    }).catch(err => {
+        res.status(500).json({message : 'server-error'});
+    });
+
+});
+//28-2 과제 목록을 요청(GET)받으면 해당 학생의 반 id에 속하는 과제 목록을 반환한다.
+router.get('/getClassAssignment/:id',function(req,res,next){
+    
+    const user_id = mongoose.Types.ObjectId(req.decoded_token._id);
+    const class_id = mongoose.Types.ObjectId(req.params.id);
+
+    model.classroom.find()
+    .where('_id').equals(class_id)
+    .then(result => {
+        return model.assignment.find()
+        .where('classroom_name').equals(result.name)
+        .where('user_id').equals(user_id);
+    }).then(result =>{
+        res.status(200).json(result);
+    }).catch(err => {
+        res.status(500).json({message : 'server-error'});
+    });
+
+});
 
  
 //20-2
 //넘어오는 body 값들 problem_number,assignment의 네임, 시작 끝 기간, classroom_id
-router.post('/setAssignment',(req,res,next) => {
+router.post('/addAssignment',(req,res,next) => {
     const user_id = mongoose.Types.ObjectId(req.decoded_token._id);
     const classroom_id = mongoose.Types.ObjectId(req.body.classroom_id);
-
+    const response = {nickname:'',classroom_name :''};
     model.user.findOne()
     .where('_id').equals(user_id)
     .then(result => {
         if(result ===null) throw new Error('invalid token');
-        const nickname = result.nickname;
-        
-        model.classroom.findOne()
-        .where('_id').equals(classroom_id)
-        .then(result => {
-            const classroom_name = result.name;
-
-            model.problem.find()
-            .where('problem_number').equals(req.body.problemnumber)
-            .then(result => {
-                const save_assignment = model.assignment({
+        response.nickname = result.nickname;
+        return model.classroom.find().where('id').equals(classroom_id);
+    }).then(result =>{ 
+        response.classroom_name = result.name;
+        const save_assignment = model.assignment({
                     user_id : user_id,
                     name  : req.body.name,
-                    problem_list : result.problem_number,
+                    problem_list : req.body.problem_number,
                     start_date : req.body.start_date,
                     end_date : req.body.end_date,
-                    classroom_name : classroom_name,
-                    teacher_nickname : nickname
-                });
-            return save_assignment.save();
+                    classroom_name : response.classroom_name,
+                    teacher_nickname : response.nickname
             });
-        });
+            return save_assignment.save();
     }).then(result => {    
         res.status(200).json({message : 'assignment is created'});
     }).catch(err => {
@@ -127,7 +151,7 @@ router.get('/getDescription/:problem_number',function(req,res,next){
     const respons = {problem_description : [] , sample_input : [], sample_output : []};
 
 
-    model.problem.findOne()
+    model.problem.find()
         .where('problem_number').equals(pro_number)
         .then(result => {
             if(result === null) throw new error('no problem has been exist');
@@ -139,12 +163,7 @@ router.get('/getDescription/:problem_number',function(req,res,next){
         }).then(result =>{
         res.status(200).json(respons);
     }).catch(err => {
-        if(err.message === 'no problem has been exist'){
-            res.status(400).json({message : 'problem do not exist'});
-        }
-        else{
         res.status(500).json({message : 'server-error'});
-        }
     });
 
 });
